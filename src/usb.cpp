5ff247fe6e6065af93e4366f8a7f6e7d8a68b9d4
Jevin Sweval <jevinsweval@gmail.com>
2022-06-12 23:37:28 -0700

    more interfaces

--- src/usb.cpp
+++ src/usb.cpp
@@ -9,19 +9,28 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(js2232_usb);
 
-#define LOOPBACK_OUT_EP_ADDR 0x01
-#define LOOPBACK_IN_EP_ADDR 0x81
+#define IF0_IN_EP_ADDR 0x81
+#define IF0_OUT_EP_ADDR 0x01
 
-#define LOOPBACK_OUT_EP_IDX 0
-#define LOOPBACK_IN_EP_IDX 1
+#define IF0_IN_EP_IDX 0
+#define IF0_OUT_EP_IDX 1
+
+#define IF1_IN_EP_ADDR 0x82
+#define IF1_OUT_EP_ADDR 0x02
+
+#define IF1_IN_EP_IDX 0
+#define IF1_OUT_EP_IDX 1
 
 static uint8_t loopback_buf[1024];
 BUILD_ASSERT(sizeof(loopback_buf) == CONFIG_USB_REQUEST_BUFFER_SIZE);
 
 struct usb_loopback_config {
     struct usb_if_descriptor if0;
-    struct usb_ep_descriptor if0_out_ep;
     struct usb_ep_descriptor if0_in_ep;
+    struct usb_ep_descriptor if0_out_ep;
+    struct usb_if_descriptor if1;
+    struct usb_ep_descriptor if1_in_ep;
+    struct usb_ep_descriptor if1_out_ep;
 } __packed;
 
 USBD_CLASS_DESCR_DEFINE(primary, 0)
@@ -35,28 +44,64 @@ struct usb_loopback_config loopback_cfg = {
             .bAlternateSetting  = 0,
             .bNumEndpoints      = 2,
             .bInterfaceClass    = USB_BCC_VENDOR,
-            .bInterfaceSubClass = 0,
-            .bInterfaceProtocol = 0,
+            .bInterfaceSubClass = 0xFF,
+            .bInterfaceProtocol = 0xFF,
             .iInterface         = 0,
         },
 
-    /* Data Endpoint OUT */
-    .if0_out_ep =
+    /* Data Endpoint IN 1 */
+    .if0_in_ep =
         {
             .bLength          = sizeof(struct usb_ep_descriptor),
             .bDescriptorType  = USB_DESC_ENDPOINT,
-            .bEndpointAddress = LOOPBACK_OUT_EP_ADDR,
+            .bEndpointAddress = IF0_IN_EP_ADDR,
             .bmAttributes     = USB_DC_EP_BULK,
             .wMaxPacketSize   = sys_cpu_to_le16(CONFIG_JS2232_BULK_EP_MPS),
             .bInterval        = 0x00,
         },
 
-    /* Data Endpoint IN */
-    .if0_in_ep =
+    /* Data Endpoint OUT 2 */
+    .if0_out_ep =
         {
             .bLength          = sizeof(struct usb_ep_descriptor),
             .bDescriptorType  = USB_DESC_ENDPOINT,
-            .bEndpointAddress = LOOPBACK_IN_EP_ADDR,
+            .bEndpointAddress = IF0_OUT_EP_ADDR,
+            .bmAttributes     = USB_DC_EP_BULK,
+            .wMaxPacketSize   = sys_cpu_to_le16(CONFIG_JS2232_BULK_EP_MPS),
+            .bInterval        = 0x00,
+        },
+
+    /* Interface descriptor 1 */
+    .if1 =
+        {
+            .bLength            = sizeof(struct usb_if_descriptor),
+            .bDescriptorType    = USB_DESC_INTERFACE,
+            .bInterfaceNumber   = 1,
+            .bAlternateSetting  = 0,
+            .bNumEndpoints      = 2,
+            .bInterfaceClass    = USB_BCC_VENDOR,
+            .bInterfaceSubClass = 0xFF,
+            .bInterfaceProtocol = 0xFF,
+            .iInterface         = 0,
+        },
+
+    /* Data Endpoint IN 3 */
+    .if1_in_ep =
+        {
+            .bLength          = sizeof(struct usb_ep_descriptor),
+            .bDescriptorType  = USB_DESC_ENDPOINT,
+            .bEndpointAddress = IF1_IN_EP_ADDR,
+            .bmAttributes     = USB_DC_EP_BULK,
+            .wMaxPacketSize   = sys_cpu_to_le16(CONFIG_JS2232_BULK_EP_MPS),
+            .bInterval        = 0x00,
+        },
+
+    /* Data Endpoint OUT 4 */
+    .if1_out_ep =
+        {
+            .bLength          = sizeof(struct usb_ep_descriptor),
+            .bDescriptorType  = USB_DESC_ENDPOINT,
+            .bEndpointAddress = IF1_OUT_EP_ADDR,
             .bmAttributes     = USB_DC_EP_BULK,
             .wMaxPacketSize   = sys_cpu_to_le16(CONFIG_JS2232_BULK_EP_MPS),
             .bInterval        = 0x00,
@@ -89,7 +134,7 @@ struct usb_common_descriptor common_desc = {
             .bLength             = sizeof(struct usb_cfg_descriptor),
             .bDescriptorType     = USB_DESC_CONFIGURATION,
             .wTotalLength        = 0,
-            .bNumInterfaces      = 0,
+            .bNumInterfaces      = 2,
             .bConfigurationValue = 1,
             .iConfiguration      = 0,
             .bmAttributes        = USB_SCD_RESERVED,
@@ -115,11 +160,19 @@ static void loopback_in_cb(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status) 
 static struct usb_ep_cfg_data ep_cfg[] = {
     {
         .ep_cb   = loopback_out_cb,
-        .ep_addr = LOOPBACK_OUT_EP_ADDR,
+        .ep_addr = IF0_OUT_EP_ADDR,
     },
     {
         .ep_cb   = loopback_in_cb,
-        .ep_addr = LOOPBACK_IN_EP_ADDR,
+        .ep_addr = IF0_IN_EP_ADDR,
+    },
+    {
+        .ep_cb   = loopback_out_cb,
+        .ep_addr = IF1_OUT_EP_ADDR,
+    },
+    {
+        .ep_cb   = loopback_in_cb,
+        .ep_addr = IF1_IN_EP_ADDR,
     },
 };
 /* usb.rst endpoint configuration end */
@@ -130,7 +183,7 @@ static void loopback_status_cb(struct usb_cfg_data *cfg, enum usb_dc_status_code
 
     switch (status) {
     case USB_DC_INTERFACE:
-        loopback_in_cb(ep_cfg[LOOPBACK_IN_EP_IDX].ep_addr, USB_DC_EP_SETUP);
+        loopback_in_cb(ep_cfg[IF0_IN_EP_IDX].ep_addr, USB_DC_EP_SETUP);
         LOG_DBG("USB interface configured");
         break;
     case USB_DC_SET_HALT:
@@ -138,8 +191,8 @@ static void loopback_status_cb(struct usb_cfg_data *cfg, enum usb_dc_status_code
         break;
     case USB_DC_CLEAR_HALT:
         LOG_DBG("Clear Feature ENDPOINT_HALT");
-        if (*param == ep_cfg[LOOPBACK_IN_EP_IDX].ep_addr) {
-            loopback_in_cb(ep_cfg[LOOPBACK_IN_EP_IDX].ep_addr, USB_DC_EP_SETUP);
+        if (*param == ep_cfg[IF0_IN_EP_IDX].ep_addr) {
+            loopback_in_cb(ep_cfg[IF0_IN_EP_IDX].ep_addr, USB_DC_EP_SETUP);
         }
         break;
     default:
