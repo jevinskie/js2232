a1514cdc0cafa437d8bba1bbe07f20704e2045e6
Jevin Sweval <jevinsweval@gmail.com>
2022-06-15 12:12:33 -0700

    loopback/in/out test modes

--- scripts/loopback_test.py
+++ scripts/loopback_test.py
@@ -4,16 +4,28 @@ import argparse
 import random
 import sys
 import time
+from enum import IntEnum
 
 import usb1
 
-PACKET_SIZE = 1024
+REQ_SET_TEST_MODE = 0x42
+REQ_SET_PACKET_SZ = 0x43
+
+
+class test_mode_t(IntEnum):
+    INVALID_MODE = 0
+    LOOPBACK_BULK = 1
+    OUT_BULK = 2
+    IN_BULK = 3
+
 
 parser = argparse.ArgumentParser(description="USB device benchmark")
 parser.add_argument("--loop-bulk", action="store_true", help="Do loopback bulk test")
 parser.add_argument("--out-bulk", action="store_true", help="Do OUT bulk test")
 parser.add_argument("--in-bulk", action="store_true", help="Do IN bulk test")
+parser.add_argument("--sz", default=1024, type=int, help="Packet size for test")
 args = parser.parse_args()
+PACKET_SIZE = args.sz
 
 with usb1.USBContext() as ctx:
     handle = None
@@ -32,28 +44,52 @@ with usb1.USBContext() as ctx:
     with handle.claimInterface(0):
         handle.clearHalt(1)
         handle.clearHalt(0x81)
+        test_mode = test_mode_t.INVALID_MODE
+        if args.loop_bulk:
+            test_mode = test_mode_t.LOOPBACK_BULK
+        elif args.out_bulk:
+            test_mode = test_mode_t.OUT_BULK
+        elif args.in_bulk:
+            test_mode = test_mode_t.IN_BULK
+        else:
+            raise RuntimeError("must select a test mode")
+        handle.controlWrite(
+            usb1.RECIPIENT_DEVICE | usb1.REQUEST_TYPE_VENDOR,
+            REQ_SET_PACKET_SZ,
+            PACKET_SIZE,
+            0,
+            b"",
+            timeout=30,
+        )
+        handle.controlWrite(
+            usb1.RECIPIENT_DEVICE | usb1.REQUEST_TYPE_VENDOR,
+            REQ_SET_TEST_MODE,
+            test_mode,
+            0,
+            b"",
+            timeout=30,
+        )
         nbytes = 0
         tstart = time.time()
         try:
             while True:
                 if args.loop_bulk:
-                    obuf = random.randbytes(PACKET_SIZE)
+                    obuf = bytearray(random.randbytes(PACKET_SIZE))
+                    print(f"obuf: {obuf.hex(' ')}")
                     handle.bulkWrite(1, obuf, timeout=1000)
                     ibuf = handle.bulkRead(1, PACKET_SIZE, timeout=1000)
                     ibuf_inv = bytes([b ^ 0xFF for b in ibuf])
+                    print(f"Ibuf: {ibuf_inv.hex(' ')}")
                     assert ibuf_inv == obuf
                     nbytes += len(obuf) * 2
-
                 elif args.out_bulk:
-                    obuf = random.randbytes(PACKET_SIZE)
+                    obuf = bytearray(random.randbytes(PACKET_SIZE))
                     handle.bulkWrite(1, obuf, timeout=1000)
                     nbytes += len(obuf)
                 elif args.in_bulk:
                     ibuf = handle.bulkRead(1, PACKET_SIZE, timeout=1000)
                     assert len(ibuf) == PACKET_SIZE
                     nbytes += len(ibuf)
-                else:
-                    raise RuntimeError("must select a test mode")
                 if nbytes % (16 * 1024) == 0:
                     print(".", end="", flush=True)
         except KeyboardInterrupt:
--- src/usb-loopback.cpp
+++ src/usb-loopback.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 
 #include <zephyr/init.h>
 
@@ -9,21 +10,30 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(js2232_usb);
 
-#define IF0_IN_EP_ADDR 0x81
-#define IF0_OUT_EP_ADDR 0x01
+constexpr int IF0_IN_EP_ADDR  = 0x81;
+constexpr int IF0_OUT_EP_ADDR = 0x01;
 
-#define IF0_IN_EP_IDX 0
-#define IF0_OUT_EP_IDX 1
+constexpr int IF0_IN_EP_IDX  = 0;
+constexpr int IF0_OUT_EP_IDX = 1;
 
-#define IF1_IN_EP_ADDR 0x82
-#define IF1_OUT_EP_ADDR 0x02
+constexpr int IF1_IN_EP_ADDR  = 0x82;
+constexpr int IF1_OUT_EP_ADDR = 0x02;
 
-#define IF1_IN_EP_IDX 3
-#define IF1_OUT_EP_IDX 4
+constexpr int IF1_IN_EP_IDX  = 3;
+constexpr int IF1_OUT_EP_IDX = 4;
 
-// #define LOOPBACK_TEST
-// #define OUT_TEST
-#define IN_TEST
+constexpr uint8_t REQ_SET_TEST_MODE = 0x42;
+constexpr uint8_t REQ_SET_PACKET_SZ = 0x43;
+
+enum test_mode_t : uint16_t {
+    INVALID_MODE,
+    LOOPBACK_BULK,
+    OUT_BULK,
+    IN_BULK,
+};
+
+test_mode_t test_mode = INVALID_MODE;
+uint16_t test_pkt_sz  = 0;
 
 static uint8_t loopback_buf[1024];
 BUILD_ASSERT(sizeof(loopback_buf) == CONFIG_USB_REQUEST_BUFFER_SIZE);
@@ -32,9 +42,6 @@ struct usb_loopback_config {
     struct usb_if_descriptor if0;
     struct usb_ep_descriptor if0_in_ep;
     struct usb_ep_descriptor if0_out_ep;
-    // struct usb_if_descriptor if1;
-    // struct usb_ep_descriptor if1_in_ep;
-    // struct usb_ep_descriptor if1_out_ep;
 } __packed;
 
 USBD_CLASS_DESCR_DEFINE(primary, 0)
@@ -74,42 +81,6 @@ struct usb_loopback_config loopback_cfg = {
             .wMaxPacketSize   = sys_cpu_to_le16(CONFIG_JS2232_BULK_EP_MPS),
             .bInterval        = 0x00,
         },
-
-    // /* Interface descriptor 1 */
-    // .if1 =
-    //     {
-    //         .bLength            = sizeof(struct usb_if_descriptor),
-    //         .bDescriptorType    = USB_DESC_INTERFACE,
-    //         .bInterfaceNumber   = 1,
-    //         .bAlternateSetting  = 0,
-    //         .bNumEndpoints      = 2,
-    //         .bInterfaceClass    = USB_BCC_VENDOR,
-    //         .bInterfaceSubClass = 0xFF,
-    //         .bInterfaceProtocol = 0xFF,
-    //         .iInterface         = 0,
-    //     },
-
-    // /* Data Endpoint IN 2 */
-    // .if1_in_ep =
-    //     {
-    //         .bLength          = sizeof(struct usb_ep_descriptor),
-    //         .bDescriptorType  = USB_DESC_ENDPOINT,
-    //         .bEndpointAddress = IF1_IN_EP_ADDR,
-    //         .bmAttributes     = USB_DC_EP_BULK,
-    //         .wMaxPacketSize   = sys_cpu_to_le16(CONFIG_JS2232_BULK_EP_MPS),
-    //         .bInterval        = 0x00,
-    //     },
-
-    // /* Data Endpoint OUT 2 */
-    // .if1_out_ep =
-    //     {
-    //         .bLength          = sizeof(struct usb_ep_descriptor),
-    //         .bDescriptorType  = USB_DESC_ENDPOINT,
-    //         .bEndpointAddress = IF1_OUT_EP_ADDR,
-    //         .bmAttributes     = USB_DC_EP_BULK,
-    //         .wMaxPacketSize   = sys_cpu_to_le16(CONFIG_JS2232_BULK_EP_MPS),
-    //         .bInterval        = 0x00,
-    //     },
 };
 
 USBD_DEVICE_DESCR_DEFINE(primary)
@@ -167,45 +138,31 @@ static void invert_buf(uint8_t *buf, uint16_t len) {
     }
 }
 
-static void loopback_out_cb(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status) {
-    uint32_t bytes_to_read;
-
-    usb_read(ep, nullptr, 0, &bytes_to_read);
-    // LOG_INF("ep 0x%x, bytes to read %d ", ep, bytes_to_read);
-    usb_read(ep, loopback_buf, bytes_to_read, nullptr);
-    invert_buf(loopback_buf, bytes_to_read);
-    if (usb_write(ep | 0x80, loopback_buf, bytes_to_read, nullptr)) {
-        LOG_INF("wtf ep 0x%x", ep);
-    }
-}
-
-static void loopback_in_cb(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status) {
-    // LOG_INF("%s try ep 0x%x", __FUNCTION__, ep);
-    // if (usb_write(ep, loopback_buf, 4, nullptr)) {
-    //     LOG_INF("ep 0x%x", ep);
-    // }
-}
-
 void xfer_cb(uint8_t ep, int tsize, void *was_in) {
     // LOG_INF("ep: %d sz: %d priv: %d", ep, tsize, (int)was_in);
-#if defined(LOOPBACK_TEST)
-    if (!was_in) {
-        invert_buf(loopback_buf, tsize);
-        usb_transfer(0x81, loopback_buf, tsize, USB_TRANS_WRITE | USB_TRANS_NO_ZLP, xfer_cb,
+    switch (test_mode) {
+    case LOOPBACK_BULK:
+        if (!was_in) {
+            invert_buf(loopback_buf, tsize);
+            usb_transfer(0x81, loopback_buf, tsize, USB_TRANS_WRITE | USB_TRANS_NO_ZLP, xfer_cb,
+                         (void *)1);
+        } else {
+            usb_transfer(1, loopback_buf, test_pkt_sz, USB_TRANS_READ | USB_TRANS_NO_ZLP, xfer_cb,
+                         (void *)0);
+        }
+        break;
+    case OUT_BULK:
+        usb_transfer(1, loopback_buf, test_pkt_sz, USB_TRANS_READ | USB_TRANS_NO_ZLP, xfer_cb,
+                     (void *)0);
+        break;
+    case IN_BULK:
+        usb_transfer(0x81, loopback_buf, test_pkt_sz, USB_TRANS_WRITE | USB_TRANS_NO_ZLP, xfer_cb,
                      (void *)1);
-    } else {
-        usb_transfer(1, loopback_buf, sizeof(loopback_buf), USB_TRANS_READ | USB_TRANS_NO_ZLP,
-                     xfer_cb, (void *)0);
+        break;
+    default:
+        assert(!"Invalid test mode");
+        break;
     }
-#elif defined(OUT_TEST)
-    usb_transfer(1, loopback_buf, sizeof(loopback_buf), USB_TRANS_READ | USB_TRANS_NO_ZLP, xfer_cb,
-                 (void *)0);
-#elif defined(IN_TEST)
-    usb_transfer(0x81, loopback_buf, sizeof(loopback_buf), USB_TRANS_WRITE | USB_TRANS_NO_ZLP,
-                 xfer_cb, (void *)1);
-#else
-#error Must define a test mode
-#endif
 }
 
 static struct usb_ep_cfg_data ep_cfg[] = {
@@ -217,14 +174,6 @@ static struct usb_ep_cfg_data ep_cfg[] = {
         .ep_cb   = usb_transfer_ep_callback,
         .ep_addr = IF0_IN_EP_ADDR,
     },
-    // {
-    //     .ep_cb   = nullptr,
-    //     .ep_addr = IF1_OUT_EP_ADDR,
-    // },
-    // {
-    //     .ep_cb   = nullptr,
-    //     .ep_addr = IF1_IN_EP_ADDR,
-    // },
 };
 
 static void loopback_status_cb(struct usb_cfg_data *cfg, enum usb_dc_status_code status,
@@ -235,7 +184,6 @@ static void loopback_status_cb(struct usb_cfg_data *cfg, enum usb_dc_status_code
 
     switch (status) {
     case USB_DC_INTERFACE:
-        loopback_in_cb(ep_cfg[IF0_IN_EP_IDX].ep_addr, USB_DC_EP_SETUP);
         LOG_INF("USB interface configured");
         break;
     case USB_DC_SET_HALT:
@@ -243,24 +191,10 @@ static void loopback_status_cb(struct usb_cfg_data *cfg, enum usb_dc_status_code
         break;
     case USB_DC_CLEAR_HALT:
         LOG_INF("Clear Feature ENDPOINT_HALT");
-        if (*param == ep_cfg[IF0_IN_EP_IDX].ep_addr) {
-            loopback_in_cb(ep_cfg[IF0_IN_EP_IDX].ep_addr, USB_DC_EP_SETUP);
-        }
         break;
     case USB_DC_CONFIGURED:
         LOG_INF("Configured");
-#if defined(LOOPBACK_TEST)
-        usb_transfer(1, loopback_buf, sizeof(loopback_buf), USB_TRANS_READ | USB_TRANS_NO_ZLP,
-                     xfer_cb, (void *)0);
-#elif defined(OUT_TEST)
-        usb_transfer(1, loopback_buf, sizeof(loopback_buf), USB_TRANS_READ | USB_TRANS_NO_ZLP,
-                     xfer_cb, (void *)0);
-#elif defined(IN_TEST)
-        usb_transfer(0x81, loopback_buf, sizeof(loopback_buf), USB_TRANS_WRITE | USB_TRANS_NO_ZLP,
-                     xfer_cb, (void *)1);
-#else
-#error Must define a test mode
-#endif
+        break;
     default:
         break;
     }
@@ -270,8 +204,47 @@ static int loopback_vendor_handler(struct usb_setup_packet *setup, int32_t *len,
     LOG_INF("Class request: bRequest 0x%x bmRequestType 0x%x len %d", setup->bRequest,
             setup->bmRequestType, *len);
 
-    if (setup->RequestType.recipient != USB_REQTYPE_RECIPIENT_DEVICE) {
+    if (setup->RequestType.recipient != USB_REQTYPE_RECIPIENT_DEVICE ||
+        setup->RequestType.type != USB_REQTYPE_TYPE_VENDOR) {
         return -ENOTSUP;
+    }
+
+    if (setup->bRequest == REQ_SET_TEST_MODE) {
+        test_mode = (test_mode_t)setup->wValue;
+        assert(test_pkt_sz);
+        usb_cancel_transfer(1);
+        usb_cancel_transfer(0x81);
+        memset(loopback_buf, 0, sizeof(loopback_buf));
+        switch (test_mode) {
+        case LOOPBACK_BULK:
+            LOG_INF("Test mode: loopback bulk");
+            usb_transfer(1, loopback_buf, test_pkt_sz, USB_TRANS_READ | USB_TRANS_NO_ZLP, xfer_cb,
+                         (void *)0);
+            break;
+        case OUT_BULK:
+            LOG_INF("Test mode: out bulk");
+            usb_transfer(1, loopback_buf, test_pkt_sz, USB_TRANS_READ | USB_TRANS_NO_ZLP, xfer_cb,
+                         (void *)0);
+            break;
+        case IN_BULK:
+            LOG_INF("Test mode: in bulk");
+            usb_transfer(0x81, loopback_buf, test_pkt_sz, USB_TRANS_WRITE | USB_TRANS_NO_ZLP,
+                         xfer_cb, (void *)1);
+            break;
+        default:
+            assert(!"Invalid test mode");
+            break;
+        }
+        return 0;
+    } else if (setup->bRequest == REQ_SET_PACKET_SZ) {
+        test_pkt_sz = setup->wValue;
+        LOG_INF("Setting test packet size to %u", test_pkt_sz);
+        if (test_pkt_sz > sizeof(loopback_buf)) {
+            LOG_ERR("Packet size: %u is larger than buffer size %zu", test_pkt_sz,
+                    sizeof(loopback_buf));
+            return -ENOTSUP;
+        }
+        return 0;
     }
 
     return -ENOTSUP;
