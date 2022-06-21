c50f60f74ca0602fc163b3a4b86ff7c1000ff4c8
Jevin Sweval <jevinsweval@gmail.com>
2022-06-20 22:28:56 -0700

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pinctrl.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/zephyr.h>
@@ -8,14 +9,23 @@ LOG_MODULE_REGISTER(main);
 #define SLEEP_TIME_MS 1000
 
 #define LED0_NODE DT_ALIAS(led0)
-
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+#define USB_NODE DT_NODELABEL(usb)
+PINCTRL_DT_DEFINE(USB_NODE);
+// #define USB_NODE DT_ALIAS(usb)
+// static const struct pinctrl_dev_config *usb_pcfg = PINCTRL_DT_DEV_CONFIG_GET(DT_NODELABEL(usb));
+
+static void usb_bus_reset() {
+    PINCTRL_DT_DEV_CONFIG_GET(USB_NODE);
+}
 
 void main(void) {
     int ret;
 
     printk("Hello World! %s\n", CONFIG_BOARD);
 
+    usb_bus_reset();
     ret = usb_enable(nullptr);
     if (ret != 0) {
         LOG_ERR("Failed to enable USB");
