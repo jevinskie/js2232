a464abb10d2ffda68cd8c969deb574de93720f77
Jevin Sweval <jevinsweval@gmail.com>
2022-06-21 18:36:57 -0700

    wip

--- scripts/usbbench.py
+++ scripts/usbbench.py
@@ -3,10 +3,21 @@
 import argparse
 import random
 import time
+from enum import IntEnum
 
 import usb1
 
 PACKET_SIZE = 64
+
+REQ_SET_TEST_MODE = 0x42
+REQ_SET_PACKET_SZ = 0x43
+
+
+class test_mode_t(IntEnum):
+    INVALID_MODE = 0
+    LOOPBACK_BULK = 1
+    OUT_BULK = 2
+    IN_BULK = 3
 
 
 def main(args):
@@ -28,19 +39,73 @@ def main(args):
             break
         assert handle
         handle.setAutoDetachKernelDriver(True)
+        cfg0 = next(dev.iterConfigurations())
+        print(f"cfg0: {cfg0}")
+        interface = cfg0[args.interface]
+        print(f"interface: {interface}")
+        setting = interface[0]
+        print(f"setting: {setting}")
+        if args.in_test:
+            in_ep = setting[0]
+            assert in_ep.getAddress() & 0x80
+        elif args.out_test:
+            out_ep = setting[0]
+            assert not out_ep.getAddress() & 0x80
+        else:
+            in_ep, out_ep = [None] * 2
+            ep_iter = iter(setting)
+            while not all((in_ep, out_ep)):
+                ep = next(ep_iter)
+                if not ep.getAddress() & 0x80:
+                    out_ep = ep
+                    out_ep_addr = ep.getAddress()
+                else:
+                    in_ep = ep
+                    in_ep_addr = ep.getAddress() ^ 0x80
+        if out_ep:
+            print(f"out_ep: {out_ep_addr:#04x}")
+        if in_ep:
+            print(f"in_ep: {in_ep_addr:#04x}")
         with handle.claimInterface(args.interface):
-            handle.clearHalt(args.out_ep)
-            handle.clearHalt(args.in_ep)
+            if out_ep:
+                handle.clearHalt(out_ep.getAddress())
+            if in_ep:
+                handle.clearHalt(in_ep.getAddress())
+            test_mode = test_mode_t.INVALID_MODE
+            if args.loop_test:
+                test_mode = test_mode_t.LOOPBACK_BULK
+            elif args.out_test:
+                test_mode = test_mode_t.OUT_BULK
+            elif args.in_test:
+                test_mode = test_mode_t.IN_BULK
+            else:
+                raise RuntimeError("must select a test mode")
+            handle.controlWrite(
+                usb1.RECIPIENT_DEVICE | usb1.REQUEST_TYPE_VENDOR,
+                REQ_SET_PACKET_SZ,
+                PACKET_SIZE,
+                0,
+                b"",
+                timeout=100,
+            )
+            handle.controlWrite(
+                usb1.RECIPIENT_DEVICE | usb1.REQUEST_TYPE_VENDOR,
+                REQ_SET_TEST_MODE,
+                test_mode,
+                0,
+                b"",
+                timeout=100,
+            )
             nbytes = 0
             tstart = time.time()
             try:
                 while True:
                     obuf = bytearray(random.randbytes(PACKET_SIZE))
-                    print(f"obuf: {obuf.hex(' ')}")
-                    handle.bulkWrite(1, obuf, timeout=1000)
-                    ibuf = handle.bulkRead(1, PACKET_SIZE, timeout=1000)
+                    # print(f"obuf: {obuf.hex(' ')}")
+                    handle.bulkWrite(out_ep_addr, obuf, timeout=1000)
+                    ibuf = handle.bulkRead(in_ep_addr, PACKET_SIZE, timeout=1000)
                     ibuf_inv = bytes([b ^ 0xFF for b in ibuf])
-                    print(f"Ibuf: {ibuf_inv.hex(' ')}")
+                    # print(f"Ibuf: {ibuf_inv.hex(' ')}")
                     assert ibuf_inv == obuf
                     nbytes += len(obuf) * 2
                     if nbytes % (16 * 1024) == 0:
@@ -61,12 +126,16 @@ def any_int(s):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("usbbench")
     mode = parser.add_mutually_exclusive_group()
-    mode.add_argument("-i", "--in", action="store_true", help="In (device-to-host) test")
-    mode.add_argument("-o", "--out", action="store_true", help="Out (host-to-device) test")
-    mode.add_argument("-l", "--loop", action="store_true", help="Loopback test")
+    mode.add_argument(
+        "-i", "--in", dest="in_test", action="store_true", help="In (device-to-host) test"
+    )
+    mode.add_argument(
+        "-o", "--out", dest="out_test", action="store_true", help="Out (host-to-device) test"
+    )
+    mode.add_argument("-l", "--loop", dest="loop_test", action="store_true", help="Loopback test")
     parser.add_argument("-v", "--vid", type=any_int, default=0x0403, help="Vendor ID")
     parser.add_argument("-p", "--pid", type=any_int, default=0x6010, help="Product ID")
-    parser.add_argument("-m", "--manufacturer", default="Tractor", help="Manufacturer match")
+    parser.add_argument("-m", "--manufacturer", default="Chewing", help="Manufacturer match")
     parser.add_argument("-P", "--product", default="js2232", help="Product match")
     parser.add_argument("-f", "--interface", type=any_int, default=0, help="Interface number")
     parser.add_argument("-S", "--pkt-sz", type=any_int, default=64, help="Packet size")
